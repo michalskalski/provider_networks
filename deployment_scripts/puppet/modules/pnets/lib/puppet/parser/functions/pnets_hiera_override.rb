3440522b7c0251675d83bcfd51361240d55a402e
Michal Skalski <michal@skalski.org>
2017-02-08 12:06:28 +0100

    Set vlan range for provider networks
    When tun segmentation is used, vlan range is not defined in ml2 config.
    Plugin must do this explicit.
    
    Signed-off-by: Michal Skalski <michal@skalski.org>

--- deployment_scripts/puppet/modules/pnets/lib/puppet/parser/functions/pnets_hiera_override.rb
+++ deployment_scripts/puppet/modules/pnets/lib/puppet/parser/functions/pnets_hiera_override.rb
@@ -1,10 +1,12 @@
 require 'yaml'
 
 module Puppet::Parser::Functions
-  newfunction(:pnets_hiera_override, :arity => 2) do |args|
-    filename, mappings = args
+  newfunction(:pnets_hiera_override, :type => :rvalue, :arity => 2) do |args|
+    mappings, segmentation = args
 
+    override = {}
     pnets_hash = {}
+    vlan_nets = []
     mappings.each do |netmap|
       netmap.gsub!(/\s+/, '')
       arr = netmap.split(',')
@@ -12,16 +14,25 @@ module Puppet::Parser::Functions
         fail("Not valid network mapping: #{netmap}")
       end
       vlan_range = (arr.size > 2 ? arr[2] : nil)
+      if segmentation == 'tun' and vlan_range
+        vlan_nets << "#{arr[0]}:#{vlan_range}"
+      end
       pnets_hash.merge!({arr[0]=>{'bridge'=>arr[1], 'vlan_range'=> vlan_range}})
     end
-
-    neutron_config = {
-      'neutron_config' => {
-        'L2' => {'phys_nets' => pnets_hash}
+    if not vlan_nets.empty?
+      override['configuration'] = {
+        'neutron_plugin_ml2' => {
+          'ml2_type_vlan/network_vlan_ranges' => {
+            'value' => vlan_nets.join(',')
+          }
+        }
       }
+    end
+
+    override['neutron_config'] = {
+      'L2' => {'phys_nets' => pnets_hash}
     }
 
-    # write to hiera override yaml file
-    File.open(filename, 'w') { |file| file.write(neutron_config.to_yaml) }
+    override.to_yaml
   end
 end
--- deployment_scripts/puppet/modules/pnets/manifests/init.pp
+++ deployment_scripts/puppet/modules/pnets/manifests/init.pp
@@ -1,8 +1,16 @@
 class pnets {
   $override_file = '/etc/hiera/plugins/provider_networks.yaml'
   $conf = hiera('provider_networks')
-  pnets_hiera_override(
-    $override_file,
-    $conf['network_list']
+  $neutron_config = hiera_hash('neutron_config')
+  $segmentation_type = try_get_value($neutron_config, 'L2/segmentation_type')
+  $pnets = pnets_hiera_override(
+    $conf['network_list'],
+    $segmentation_type
   )
+  file { $override_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    content => $pnets,
+  }
 }
